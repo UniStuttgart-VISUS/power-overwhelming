272f86666e94c565bb3b97d782d9bd2f3236ad29
mueller <christoph.mueller@visus.uni-stuttgart.de>
2021-12-07 10:00:23 +0100

    Added visa_exception.

---
+++ power_overwhelming/visa_exception.cpp
@@ -0,0 +1,13 @@
+// <copyright file="visa_exception.cpp" company="Visualisierungsinstitut der Universität Stuttgart">
+// Copyright © 2021 Visualisierungsinstitut der Universität Stuttgart. Alle Rechte vorbehalten.
+// </copyright>
+// <author>Christoph Müller</author>
+
+#include "visa_exception.h"
+
+
+/*
+ * visus::power_overwhelming::visa_exception::visa_exception
+ */
+visus::power_overwhelming::visa_exception::visa_exception(const value_type code)
+        : std::exception(std::to_string(code).c_str()), _code(code) { }
