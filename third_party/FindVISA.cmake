697ff3d3567998f777b526b4d46cf27c1bde1fa6
Christoph Müller <christoph.mueller@visus.uni-stuttgart.de>
2023-02-28 18:24:41 +0100

    Fix VISA detection.

--- third_party/FindVISA.cmake
+++ third_party/FindVISA.cmake
@@ -4,31 +4,28 @@
 include(FindPackageHandleStandardArgs)
 
 
-# Potential installation locations of VISA
-set(VisaDirs "$ENV{ProgramFiles}/IVI Foundation/VISA" "$ENV{ProgramFiles\(x86\)}/IVI Foundation/VISA")
-set(VisaDirs "$ENV{ProgramFiles\(x86\)}/IVI Foundation/VISA")
-message(STATUS "Searching VISA library in ${VisaDirs} ...")
-
-# Determine the name of the library for the current platform
+# Determine where the platform-dependent library location is.
 if ("${CMAKE_SIZEOF_VOID_P}" STREQUAL "4")
-    set(VisaLibrary "visa32.lib")
-else ("${CMAKE_SIZEOF_VOID_P}" STREQUAL "4")
-    set(VisaLibrary "visa64.lib")
-endif ("${CMAKE_SIZEOF_VOID_P}" STREQUAL "4")
+    set(VisaDirs "$ENV{ProgramFiles\(x86\)}/IVI Foundation/VISA")
+else ()
+    set(VisaDirs "$ENV{ProgramFiles}/IVI Foundation/VISA")
+endif ()
+
+message(STATUS "Searching VISA library in ${VisaDirs} ...")
 
 # Find location of public headers
 find_path(VISA_INCLUDE_DIR
     NAMES visa.h
     PATHS ${VisaDirs}
-    PATH_SUFFIXES "WinNT/Include" "Win64/Include"
+    PATH_SUFFIXES "WinNT/RsVisa/include" "Win64/RsVisa/include"
 )
 mark_as_advanced(VISA_INCLUDE_DIR)
 
 # Find the library
 find_library(VISA_LIBRARY
-    NAMES ${VisaLibrary}
+    NAMES visa32.lib
     PATHS ${VisaDirs}
-    PATH_SUFFIXES "WinNT/lib/msc" "WinNT/Lib_x64/msc" "Win64/Lib_x64/msc"
+    PATH_SUFFIXES "WinNT/RsVisa/Lib/msc" "Win64/RsVisa/Lib/msc"
 )
 mark_as_advanced(VISA_LIBRARY)
 
