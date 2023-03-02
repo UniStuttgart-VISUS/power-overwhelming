5fa1d46dec9363bfaf83f179a647d69b5c50b809
Christoph Mueller <mueller@cadintulnic.visus.uni-stuttgart.de>
2023-03-02 19:06:23 +0100

    Linux compatibiliy

---
+++ power_overwhelming/src/zero_memory.h
@@ -0,0 +1,28 @@
+// <copyright file="zero_memory.h" company="Visualisierungsinstitut der Universität Stuttgart">
+// Copyright © 2023 Visualisierungsinstitut der Universität Stuttgart. Alle Rechte vorbehalten.
+// </copyright>
+// <author>Christoph Müller</author>
+
+#pragma once
+
+#if defined(_WIN32)
+#include <Windows.h>
+
+#else /* defined(_WIN32) */
+#include <cstddef>
+#include <cstring>
+
+
+/// <summary>
+/// Fills a block of memory with zeros.
+/// </summary>
+/// <param name="ptr">A pointer to the block that will be filled with zeros.
+/// </param>
+/// <param name="cnt">The size of the block to be filled with zeros in bytes.
+/// </param>
+inline void ZeroMemory(void *ptr, const std::size_t cnt) {
+    ::memset(ptr, 0, cnt);
+}
+
+#endif /* defined(_WIN32) */
+
