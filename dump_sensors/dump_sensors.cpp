7ae7d52b8a966ff6374a4e0eda0f229467dc2b45
Christoph Müller <christoph.mueller@visus.uni-stuttgart.de>
2023-03-08 15:58:08 +0100

    Added the option to create the collector template from dump_sensors.

--- dump_sensors/dump_sensors.cpp
+++ dump_sensors/dump_sensors.cpp
@@ -6,7 +6,9 @@
 #include "power_overwhelming/collector.h"
 #include "power_overwhelming/dump_sensors.h"
 
+#include <algorithm>
 #include <iostream>
+#include <vector>
 
 #if defined(_WIN32)
 #include <Windows.h>
@@ -16,6 +18,7 @@
 #if !defined(_tmain)
 #define _tmain main
 #define TCHAR char
+#define _T(x) (x)
 #endif /* !defined(_tmain) */
 
 
@@ -27,33 +30,53 @@
 /// <param name="argv"></param>
 /// <returns></returns>
 int _tmain(const int argc, const TCHAR **argv) {
+    using namespace visus::power_overwhelming;
+
     std::wcout << L"dump_sensors" << std::endl;
     std::wcout << L"© 2023 Visualisierungsinstitut der Universität Stuttgart."
         << std::endl << L"All rights reserved."
         << std::endl << std::endl;
 
+    const std::vector<std::basic_string<TCHAR>> cmd_line(argv, argv + argc);
     auto show_help = (argc < 2);
+
+    const auto collector = std::find(cmd_line.begin(), cmd_line.end(),
+        _T("--collector"));
+    if (collector != cmd_line.end()) {
+        // If the --collector option is specified, the file must be after it.
+        show_help = ((collector + 1) == cmd_line.end());
+    }
 
     if (show_help) {
         // Input is wrong, so show the help.
         std::wcout << L"Dumps the definition of all sensors that are currently "
             << L"available on this machine " << std::endl
             << L"into a JSON file." << std::endl << std::endl;
-        std::wcout << "Usage: dump_sensors <output path>" << std::endl;
+        std::wcout << "Usage: dump_sensors [--collector] <output path>"
+            << std::endl;
         return -2;
-
-    } else {
-        // Can go on with the real thing, which is just calling into the
-        // library to save the sensor definitions.
-        try {
-            auto cnt = visus::power_overwhelming::dump_sensors(argv[1]);
-            std::wcout << cnt << ((cnt == 1) ? L" sensor" : L" sensors")
-                << L" dumped." << std::endl;
-            return 0;
-        } catch (std::exception& ex) {
-            std::cout << ex.what() << std::endl;
-            return -1;
-        }
     }
 
+    // Can go on with the real thing, which is just calling into the library to
+    // save the sensor definitions.
+    try {
+        if (collector != cmd_line.end()) {
+            const auto path0 = *(collector + 1);
+            const auto path = convert_string<wchar_t>(path0);
+            collector::make_configuration_template(path.c_str());
+            std::wcout << L"Collector configuration template dumped to \""
+                << path << L"\"." << std::endl;
+
+        } else {
+            const auto path = cmd_line[1];
+            const auto cnt = dump_sensors(path);
+            std::wcout << cnt << ((cnt == 1) ? L" sensor" : L" sensors")
+                << L" dumped to \"" << path.c_str() << L"\"." << std::endl;
+        }
+
+        return 0;
+    } catch (std::exception& ex) {
+        std::cout << ex.what() << std::endl;
+        return -1;
+    }
 }
--- power_overwhelming/include/power_overwhelming/dump_sensors.h
+++ power_overwhelming/include/power_overwhelming/dump_sensors.h
@@ -6,6 +6,7 @@
 #pragma once
 
 #include <cstddef>
+#include <string>
 
 #include "power_overwhelming/power_overwhelming_api.h"
 
@@ -32,6 +33,19 @@ namespace power_overwhelming {
     /// <exception cref="std::ios_base::failure">If saving the sensor data
     /// failed.</exception>
     std::size_t POWER_OVERWHELMING_API dump_sensors(const wchar_t *path);
+
+    /// <summary>
+    /// Saves a JSON array of all sensors currently found on the the system.
+    /// </summary>
+    /// <param name="path">The path of the file to write the sensor data to.
+    /// </param>
+    /// <returns>The number of sensors actually dumped.</returns>
+    /// <exception cref="std::ios_base::failure">If saving the sensor data
+    /// failed.</exception>
+    template<class TChar>
+    inline std::size_t dump_sensors(const std::basic_string<TChar>& path) {
+        return dump_sensors(path.c_str());
+    }
 
 } /* namespace power_overwhelming */
 } /* namespace visus */
