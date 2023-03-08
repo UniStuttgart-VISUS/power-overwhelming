ec68d08caabe1703825c76e362dedf391327f6fa
Christoph Müller <christoph.mueller@visus.uni-stuttgart.de>
2023-03-08 10:51:20 +0100

    Linux fix.

--- power_overwhelming/src/convert_string.cpp
+++ power_overwhelming/src/convert_string.cpp
@@ -19,10 +19,11 @@
 std::size_t visus::power_overwhelming::detail::convert_string(
         char *output, const std::size_t cnt_output,
         const wchar_t *input, const std::size_t cnt_input) {
-    std::size_t retval = 0;
     auto state = std::mbstate_t();
 
 #if defined(_WIN32)
+    std::size_t retval = 0;
+
     auto error = ::wcsrtombs_s(&retval,
         output, (output == nullptr) ? 0 : cnt_output,
         &input, cnt_input,
@@ -54,10 +55,11 @@ std::size_t visus::power_overwhelming::detail::convert_string(
 std::size_t visus::power_overwhelming::detail::convert_string(
         wchar_t *output, const std::size_t cnt_output,
         const char *input, const std::size_t cnt_input) {
-    std::size_t retval = 0;
     auto state = std::mbstate_t();
 
 #if defined(_WIN32)
+    std::size_t retval = 0;
+
     auto error = ::mbsrtowcs_s(&retval,
         output, (output == nullptr) ? 0 : cnt_output,
         &input, cnt_input,
