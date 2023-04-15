60ea3724ea1a06abe2ff82bdabcf1d65587f0f11
mueller <christoph.mueller@visus.uni-stuttgart.de>
2023-04-15 17:24:46 +0200

    Added the ability to determine the CPU vendor.

--- power_overwhelming/include/power_overwhelming/cpu_vendor.h
+++ power_overwhelming/include/power_overwhelming/cpu_vendor.h
@@ -5,6 +5,8 @@
 
 #pragma once
 
+#include "power_overwhelming/power_overwhelming_api.h"
+
 
 namespace visus {
 namespace power_overwhelming {
@@ -30,10 +32,58 @@ namespace power_overwhelming {
         amd,
 
         /// <summary>
+        /// Identifies a Centaur CPU.
+        /// </summary>
+        centaur,
+
+        /// <summary>
+        /// Identifies a Cyrix CPU.
+        /// </summary>
+        cyrix,
+
+        /// <summary>
         /// Identifies an Intel CPU.
         /// </summary>
         intel,
+
+        /// <summary>
+        /// Identifies a National Semiconductor CPU.
+        /// </summary>
+        national_semiconductor,
+
+        /// <summary>
+        /// Identifies a NexGen CPU.
+        /// </summary>
+        nexgen,
+
+        /// <summary>
+        /// Identifies a Rise Technology CPU.
+        /// </summary>
+        rise,
+
+        /// <summary>
+        /// Identifies an SiS CPU.
+        /// </summary>
+        sis,
+
+        /// <summary>
+        /// Identifies a Transmeta CPU.
+        /// </summary>
+        transmeta,
+
+        /// <summary>
+        /// Identifies a UMC CPU.
+        /// </summary>
+        umc
     };
+
+    /// <summary>
+    /// Determines the vendor of the CPUs on the system.
+    /// </summary>
+    /// <returns>An enumeration value identifying the CPU vendor or
+    /// <see cref="cpu_vendor::unknown" /> if the CPU vendor could not be
+    /// determined.</returns>
+    extern POWER_OVERWHELMING_API cpu_vendor get_cpu_vendor(void) noexcept;
 
 } /* namespace power_overwhelming */
 } /* namespace visus */
---
+++ power_overwhelming/src/cpu_vendor.cpp
@@ -0,0 +1,73 @@
+﻿// <copyright file="cpu_vendor.cpp" company="Visualisierungsinstitut der Universität Stuttgart">
+// Copyright © 2023 Visualisierungsinstitut der Universität Stuttgart. Alle Rechte vorbehalten.
+// </copyright>
+// <author>Christoph Müller</author>
+
+#include "power_overwhelming/cpu_vendor.h"
+
+#include <array>
+#include <cstring>
+#include <stdexcept>
+
+#if defined(_WIN32)
+#include <intrin.h>
+#include <Windows.h>
+#else /* defined(_WIN32) */
+#include <cpuid.h>
+#endif /* defined(_WIN32) */
+
+
+/*
+ * visus::power_overwhelming::get_cpu_vendor
+ */
+visus::power_overwhelming::cpu_vendor visus::power_overwhelming::get_cpu_vendor(
+        void) noexcept {
+#define _RET_MATCH(v, r) if (::strncmp(vendor, (v), sizeof(vendor)) == 0) \
+    return cpu_vendor::r
+
+    std::array<int, 4> data;
+    char vendor[0x20] = { 0 };
+
+#if defined(_WIN32)
+    // Determine the number of functions available as described on
+    // https://learn.microsoft.com/de-de/cpp/intrinsics/cpuid-cpuidex?view=msvc-170
+    ::__cpuid(data.data(), 0);
+#else /* defined(_WIN32) */
+    // Determine the number of functions available as described on
+    // https://stackoverflow.com/questions/14266772/how-do-i-call-cpuid-in-linux
+    data[0] = ::__get_cpuid_max(0, nullptr);
+#endif /* defined(_WIN32) */
+
+    if (data[0] > 0) {
+        // If available, get the first function, which holds the name.
+#if defined(_WIN32)
+        ::__cpuidex(data.data(), 0, 0);
+#else /* defined(_WIN32) */
+        if (!::__get_cpuid(0, &data[0], &data[1], &data[2], &data[3])) {
+            return cpu_vendor::unknown;
+        }
+#endif /* defined(_WIN32) */
+
+        // Kansas city shuffle ...
+        *reinterpret_cast<int *>(vendor) = data[1];
+        *reinterpret_cast<int *>(vendor + 4) = data[3];
+        *reinterpret_cast<int *>(vendor + 8) = data[2];
+    }
+
+    // Check for common vendors from https://www.sandpile.org/x86/cpuid.htm ...
+    _RET_MATCH("AuthenticAMD", amd);
+    _RET_MATCH("GenuineIntel", intel);
+
+    // ... and the weird stuff.
+    _RET_MATCH("CentaurHauls", centaur);
+    _RET_MATCH("CyrixInstead", cyrix);
+    _RET_MATCH("Geode by NSC", national_semiconductor);
+    _RET_MATCH("NexGenDriven", nexgen);
+    _RET_MATCH("RiseRiseRise", rise);
+    _RET_MATCH("SiS SiS SiS ", sis);
+    _RET_MATCH("GenuineTMx86", transmeta);
+    _RET_MATCH("UMC UMC UMC ", umc);
+
+    return cpu_vendor::unknown;
+#undef _RET_MATCH
+}
