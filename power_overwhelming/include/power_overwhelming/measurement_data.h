7a540375b055b06c34a5e303de322fc588632db1
Christoph Müller <christoph.mueller@visus.uni-stuttgart.de>
2023-06-25 11:38:36 +0200

    Added new measurement_data container.

---
+++ power_overwhelming/include/power_overwhelming/measurement_data.h
@@ -0,0 +1,174 @@
+﻿// <copyright file="measurement_data.h" company="Visualisierungsinstitut der Universität Stuttgart">
+// Copyright © 2023 Visualisierungsinstitut der Universität Stuttgart. Alle Rechte vorbehalten.
+// </copyright>
+// <author>Christoph Müller</author>
+
+#pragma once
+
+#include <cinttypes>
+#include <utility>
+
+#include "power_overwhelming/power_overwhelming_api.h"
+
+
+namespace visus {
+namespace power_overwhelming {
+
+    /// <summary>
+    /// Container for the raw measurement data of a single sample for current,
+    ///  voltage and power.
+    /// </summary>
+    /// <remarks>
+    /// This class has been newly introduced to reduce the overhead of code that
+    /// manually tracks the source of samples instead of relying on the sensor
+    /// embedding its name into a <see cref="measurement" /> sample.
+    /// </remarks>
+    class POWER_OVERWHELMING_API measurement_data final {
+
+    public:
+
+        /// <summary>
+        /// The type of a timestamp associated with a measurement.
+        /// </summary>
+        typedef std::int64_t timestamp_type;
+
+        /// <summary>
+        /// The type of current and voltage measurements.
+        /// </summary>
+        typedef float value_type;
+
+        /// <summary>
+        /// The value used to represent invalid measurements.
+        /// </summary>
+        static const value_type invalid_value;
+
+        /// <summary>
+        /// Initialises a new instance.
+        /// </summary>
+        /// <param name="timestamp">The timestamp of the sensor. The unit is
+        /// defined by the configuration of the sensor.</param>
+        /// <param name="voltage">The measured voltage in Volts.</param>
+        /// <param name="current">The measured current in Amperes.</param>
+        /// <param name="power">The measured power in Watts.</param>
+        measurement_data(_In_ const timestamp_type timestamp,
+            _In_ const value_type voltage,
+            _In_ const value_type current,
+            _In_ const value_type power);
+
+        /// <summary>
+        /// Initialises a new instance.
+        /// </summary>
+        /// <param name="timestamp">The timestamp of the sensor. The unit is
+        /// defined by the configuration of the sensor.</param>
+        /// <param name="voltage">The measured voltage in Volts.</param>
+        /// <param name="current">The measured current in Amperes.</param>
+        /// <exception cref="std::invalid_argument">If any of
+        /// <paramref name="voltage" /> or <paramref name="current" /> is
+        /// <see cref="measurement_data::invalid_value" />.</exception>
+        measurement_data(_In_ const timestamp_type timestamp,
+            _In_ const value_type voltage,
+            _In_ const value_type current);
+
+        /// <summary>
+        /// Initialises a new instance.
+        /// </summary>
+        /// <param name="timestamp">The timestamp of the sensor. The unit is
+        /// defined by the configuration of the sensor.</param>
+        /// <param name="power"></param>
+        /// <param name="power">The measured power in Watts.</param>
+        /// <exception cref="std::invalid_argument">If <paramref name="power" />
+        /// is <see cref="measurement_data::invalid_value" />.</exception>
+        measurement_data(_In_ const timestamp_type timestamp,
+            _In_ const value_type power);
+
+        /// <summary>
+        /// Clone <paramref name="rhs" />.
+        /// </summary>
+        /// <param name="rhs">The object to be cloned.</param>
+        measurement_data(const measurement_data& rhs) = default;
+
+        /// <summary>
+        /// Move <paramref name="rhs" /> to a new object.
+        /// </summary>
+        /// <param name="rhs">The object to be moved.</param>
+        inline measurement_data(measurement_data&& rhs) noexcept {
+            *this = std::move(rhs);
+        }
+
+        /// <summary>
+        /// Finalises the instance.
+        /// </summary>
+        ~measurement_data(void) = default;
+
+        /// <summary>
+        /// Gets the electric current (in Amperes) measured at the given point
+        /// in time.
+        /// </summary>
+        /// <returns></returns>
+        inline value_type current(void) const noexcept {
+            return this->_current;
+        }
+
+        /// <summary>
+        /// Gets the electric power (in Watts) measured at the given point in
+        /// time.
+        /// </summary>
+        /// <remarks>
+        /// If the specified power is negative, it is considered as not given
+        /// and computed from current and voltage on the fly.
+        /// </remarks>
+        /// <returns></returns>
+        inline value_type power(void) const noexcept {
+            return (this->_power > static_cast<value_type>(0))
+                ? this->_power
+                : this->_current * this->_voltage;
+        }
+
+        /// <summary>
+        /// Gets the timestamp of the measurement.
+        /// </summary>
+        /// <returns></returns>
+        inline timestamp_type timestamp(void) const noexcept {
+            return this->_timestamp;
+        }
+
+        /// <summary>
+        /// Gets the electric potential (in Volts) measured at the given point
+        /// in time.
+        /// </summary>
+        /// <returns></returns>
+        inline value_type voltage(void) const noexcept {
+            return this->_voltage;
+        }
+
+        /// <summary>
+        /// Assignment.
+        /// </summary>
+        /// <param name="rhs"></param>
+        /// <returns><c>*this</c></returns>
+        measurement_data& operator =(const measurement_data& rhs) = default;
+
+        /// <summary>
+        /// Move assignment.
+        /// </summary>
+        /// <param name="rhs"></param>
+        /// <returns><c>*this</c></returns>
+        measurement_data& operator =(measurement_data&& rhs) noexcept;
+
+        /// <summary>
+        /// Test whether the measurement is valid.
+        /// </summary>
+        /// <returns><c>true</c> if the object contains valid measurement
+        /// data, <c>false</c> otherwise.</returns>
+        operator bool(void) const noexcept;
+
+    private:
+
+        value_type _current;
+        value_type _power;
+        timestamp_type _timestamp;
+        value_type _voltage;
+    };
+
+} /* namespace power_overwhelming */
+} /* namespace visus */
---
+++ power_overwhelming/src/measurement_data.cpp
@@ -0,0 +1,96 @@
+﻿// <copyright file="measurement_data.cpp" company="Visualisierungsinstitut der Universität Stuttgart">
+// Copyright © 2023 Visualisierungsinstitut der Universität Stuttgart. Alle Rechte vorbehalten.
+// </copyright>
+// <author>Christoph Müller</author>
+
+#include "power_overwhelming/measurement_data.h"
+
+#include <limits>
+#include <stdexcept>
+
+
+/*
+ * visus::power_overwhelming::measurement_data::invalid_value
+ */
+const visus::power_overwhelming::measurement_data::value_type
+visus::power_overwhelming::measurement_data::invalid_value
+    = std::numeric_limits<value_type>::lowest();
+
+
+/*
+ * visus::power_overwhelming::measurement_data::measurement_data
+ */
+visus::power_overwhelming::measurement_data::measurement_data(
+        _In_ const timestamp_type timestamp,
+        _In_ const value_type voltage,
+        _In_ const value_type current,
+        _In_ const value_type power)
+    :_current(current), _power(power), _timestamp(timestamp),
+        _voltage(voltage) { }
+
+
+/*
+ * visus::power_overwhelming::measurement_data::measurement_data
+ */
+visus::power_overwhelming::measurement_data::measurement_data(
+        _In_ const timestamp_type timestamp,
+        _In_ const value_type voltage,
+        _In_ const value_type current)
+    :_current(current), _power(invalid_value), _timestamp(timestamp),
+        _voltage(voltage) {
+    if (voltage == invalid_value) {
+        throw std::invalid_argument("A valid voltage measurement_data must be "
+            "specified.");
+    }
+    if (current == invalid_value) {
+        throw std::invalid_argument("A valid current measurement_data must be "
+            "specified.");
+    }
+}
+
+
+/*
+ * visus::power_overwhelming::measurement_data::measurement_data
+ */
+visus::power_overwhelming::measurement_data::measurement_data(
+        _In_ const timestamp_type timestamp,
+        _In_ const value_type power)
+    : _current(invalid_value), _power(power), _timestamp(timestamp),
+        _voltage(invalid_value) {
+    if (power == invalid_value) {
+        throw std::invalid_argument("A valid power measurement_data must be "
+            "specified.");
+    }
+}
+
+
+/*
+ * visus::power_overwhelming::measurement_data::operator =
+ */
+visus::power_overwhelming::measurement_data&
+visus::power_overwhelming::measurement_data::operator =(
+        measurement_data&& rhs) noexcept {
+    if (this != std::addressof(rhs)) {
+        this->_current = rhs._current;
+        rhs._current = invalid_value;
+        this->_power = rhs._power;
+        rhs._power = invalid_value;
+        this->_timestamp = rhs._timestamp;
+        rhs._timestamp = 0;
+        this->_voltage = rhs._voltage;
+        rhs._voltage = invalid_value;
+    }
+
+    return *this;
+}
+
+
+/*
+ * visus::power_overwhelming::measurement_data::operator bool
+ */
+visus::power_overwhelming::measurement_data::operator bool(void) const noexcept {
+    auto isPowerValid = (this->_power != invalid_value);
+    auto isSeparateValid = (this->_current != invalid_value)
+        && (this->_voltage != invalid_value);
+    return (isPowerValid || isSeparateValid);
+}
