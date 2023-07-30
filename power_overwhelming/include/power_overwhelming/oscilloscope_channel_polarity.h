e2dbc8bc1877a1ae6d2a9d75d3ff1fe0309e5d56
mueller <christoph.mueller@visus.uni-stuttgart.de>
2023-07-30 17:16:33 +0200

    Serialisation of channel enums

--- power_overwhelming/include/power_overwhelming/oscilloscope_channel_polarity.h
+++ power_overwhelming/include/power_overwhelming/oscilloscope_channel_polarity.h
@@ -5,6 +5,8 @@
 
 #pragma once
 
+#include "power_overwhelming/power_overwhelming_api.h"
+
 
 namespace visus {
 namespace power_overwhelming {
@@ -29,6 +31,29 @@ namespace power_overwhelming {
         /// </remarks>
         inverted,
     };
+
+    /// <summary>
+    /// Parse the given string as <see cref="oscilloscope_channel_polarity" />.
+    /// </summary>
+    /// <param name="str">The string to be parsed.</param>
+    /// <returns>The parsed enumeration value.</returns>
+    /// <exception cref="std::invalid_argument">If <paramref name="str" /> is
+    /// <c>nullptr</c>.</exception>
+    /// <exception cref="std::invalid_argument">If <paramref name="str" /> does
+    /// not represent a valid enumeration member.<exception>
+    extern POWER_OVERWHELMING_API oscilloscope_channel_polarity
+    parse_oscilloscope_channel_polarity(_In_z_ const wchar_t *str);
+
+    /// <summary>
+    /// Convert the given channel polarity to a human-readable string
+    /// representation.
+    /// </summary>
+    /// <param name="polarity">The polarity to be converted.</param>
+    /// <returns>The string representation of teh value.</returns>
+    /// <exception cref="std::invalid_argument">If the input is not
+    /// valid and therefore could not be converted.</exception>
+    extern POWER_OVERWHELMING_API _Ret_valid_ const wchar_t *to_string(
+        _In_ const oscilloscope_channel_polarity polarity);
 
 } /* namespace power_overwhelming */
 } /* namespace visus */
