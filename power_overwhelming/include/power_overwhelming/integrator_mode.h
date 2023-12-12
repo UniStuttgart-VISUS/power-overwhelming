26e52ba653cb60ed61ef37f648eea2680d983fe7
Christoph Müller <christoph.mueller@visus.uni-stuttgart.de>
2023-12-12 21:24:40 +0100

    Added integrator_mode.

---
+++ power_overwhelming/include/power_overwhelming/integrator_mode.h
@@ -0,0 +1,35 @@
+﻿// <copyright file="integrator_mode.h" company="Visualisierungsinstitut der Universität Stuttgart">
+// Copyright © 2023 Visualisierungsinstitut der Universität Stuttgart. Alle Rechte vorbehalten.
+// </copyright>
+// <author>Christoph Müller</author>
+
+#pragma once
+
+
+namespace visus {
+namespace power_overwhelming {
+
+    /// <summary>
+    /// Defines possible modes of integrator of the the
+    /// <see cref="hmc8015_sensor" />.
+    /// </summary>
+    enum class integrator_mode {
+
+        /// <summary>
+        /// Manual integration (user triggered). This is the default behaviour.
+        /// </summary>
+        manual,
+
+        /// <summary>
+        /// Integrate for a specified duration.
+        /// </summary>
+        duration,
+
+        /// <summary>
+        /// Integrate for a specified time span (start and end).
+        /// </summary>
+        time_span,
+    };
+
+} /* namespace power_overwhelming */
+} /* namespace visus */
