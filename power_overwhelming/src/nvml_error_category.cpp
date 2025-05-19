9632fafa98cdc2084088cfcf03c604fdc0b4fb63
Christoph Müller <christoph.mueller@visus.uni-stuttgart.de>
2025-05-19 18:36:36 +0200

    Added nvml_error_category.

---
+++ power_overwhelming/src/nvml_error_category.cpp
@@ -0,0 +1,58 @@
+﻿// <copyright file="nvml_error_category.cpp" company="Visualisierungsinstitut der Universität Stuttgart">
+// Copyright © 2022 - 2025 Visualisierungsinstitut der Universität Stuttgart.
+// Licensed under the MIT licence. See LICENCE file for details.
+// </copyright>
+// <author>Christoph Müller</author>
+
+#if defined(POWER_OVERWHELMING_WITH_NVML)
+#include "nvml_error_category.h"
+
+#include "nvidia_management_library.h"
+
+
+/*
+ * PWROWG_DETAIL_NAMESPACE::nvml_error_category::default_error_condition
+ */
+std::error_condition
+PWROWG_DETAIL_NAMESPACE::nvml_error_category::default_error_condition(
+        int status) const noexcept {
+    return std::error_condition(status, nvml_category());
+}
+
+
+/*
+ * PWROWG_DETAIL_NAMESPACE::message
+ */
+std::string PWROWG_DETAIL_NAMESPACE::nvml_error_category::message(
+        int status) const {
+    return nvidia_management_library::instance().nvmlErrorString(
+        static_cast<nvmlReturn_t>(status));
+}
+
+
+/*
+ * PWROWG_DETAIL_NAMESPACE::nvml_category
+ */
+const std::error_category& PWROWG_DETAIL_NAMESPACE::nvml_category(
+        void) noexcept {
+    static const nvml_error_category retval;
+    return retval;
+}
+
+
+/*
+ * PWROWG_DETAIL_NAMESPACE::throw_if_nvml_failed
+ */
+void PWROWG_DETAIL_NAMESPACE::throw_if_nvml_failed(
+        _In_ const nvmlReturn_t status,
+        _In_opt_z_ const char *message) {
+    if (status != NVML_SUCCESS) {
+        if (message == nullptr) {
+            throw std::system_error(status, nvml_category());
+        } else {
+            throw std::system_error(status, nvml_category(), message);
+        }
+    }
+}
+
+#endif /* defined(POWER_OVERWHELMING_WITH_NVML) */
--- power_overwhelming/src/nvml_error_category.h
+++ power_overwhelming/src/nvml_error_category.h
@@ -1,27 +1,27 @@
-﻿// <copyright file="nvapi_error_category.h" company="Visualisierungsinstitut der Universität Stuttgart">
+﻿// <copyright file="nvml_error_category.h" company="Visualisierungsinstitut der Universität Stuttgart">
 // Copyright © 2025 Visualisierungsinstitut der Universität Stuttgart.
 // Licensed under the MIT licence. See LICENCE file for details.
 // </copyright>
 // <author>Christoph Müller</author>
 
-#if !defined(_PWROWG_NVAPI_ERROR_CATEGORY_H)
-#define _PWROWG_NVAPI_ERROR_CATEGORY_H
+#if !defined(_PWROWG_NVML_ERROR_CATEGORY_H)
+#define _PWROWG_NVML_ERROR_CATEGORY_H
 #pragma once
 
 #include <system_error>
 
 #include "visus/pwrowg/api.h"
 
-#if defined(POWER_OVERWHELMING_WITH_NVAPI)
-#include <nvapi.h>
+#if defined(POWER_OVERWHELMING_WITH_NVML)
+#include <nvml.h>
 
 
 PWROWG_DETAIL_NAMESPACE_BEGIN
 
 /// <summary>
-/// An error category for <see cref="NvAPI_Status" />.
+/// An error category for <see cref="nvmlReturn_t" />.
 /// </summary>
-class PWROWG_TEST_API nvapi_error_category final : public std::error_category {
+class PWROWG_TEST_API nvml_error_category final : public std::error_category {
 
 public:
 
@@ -31,7 +31,7 @@ public:
     /// Converts the error code into a portable description.
     /// </summary>
     /// <param name="status">The error code to be converted.</param>
-    /// <returns>The portable description of the NVAPI error.</returns>
+    /// <returns>The portable description of the NVML error.</returns>
     std::error_condition default_error_condition(
         _In_ int status) const noexcept override;
 
@@ -48,17 +48,17 @@ public:
     /// </summary>
     /// <returns>The name of the category.</returns>
     inline const char *name(void) const noexcept override {
-        return "NVAPI";
+        return "NVML";
     }
 };
 
 
 /// <summary>
-/// Answer the one and only <see cref="nvapi_error_category" />.
+/// Answer the one and only <see cref="nvml_error_category" />.
 /// </summary>
-/// <returns>The only instance of <see cref="nvapi_error_category" />.
+/// <returns>The only instance of <see cref="nvml_error_category" />.
 /// </returns>
-PWROWG_TEST_API const std::error_category& nvapi_category(void) noexcept;
+PWROWG_TEST_API const std::error_category& nvml_category(void) noexcept;
 
 /// <summary>
 /// Check whether <paramref name="status" /> indicates failure, and if so,
@@ -69,7 +69,7 @@ PWROWG_TEST_API const std::error_category& nvapi_category(void) noexcept;
 /// exception.</param>
 /// <exception cref="std::system_error">If <paramref name="status" /> indicates
 /// failure.</exception>
-PWROWG_TEST_API void throw_if_nvapi_failed(_In_ const NvAPI_Status status,
+PWROWG_TEST_API void throw_if_nvml_failed(_In_ const nvmlReturn_t status,
     _In_opt_z_ const char *message = nullptr);
 
 PWROWG_DETAIL_NAMESPACE_END
@@ -78,22 +78,22 @@ PWROWG_DETAIL_NAMESPACE_END
 namespace std {
 
     /// <summary>
-    /// Tell STL that <see cref="NvAPI_Status" /> is an error code enumeration.
+    /// Tell STL that <see cref="nvmlReturn_t" /> is an error code enumeration.
     /// </summary>
-    template<> struct is_error_code_enum<NvAPI_Status> : true_type { };
+    template<> struct is_error_code_enum<nvmlReturn_t> : true_type { };
 
     /// <summary>
-    /// Allow STL to convert a <see cref="NvAPI_Status" /> into a generic error
+    /// Allow STL to convert a <see cref="nvmlReturn_t" /> into a generic error
     /// code.
     /// </summary>
     /// <param name="e">The NVAPI status to be converted.</param>
     /// <returns>The generic error code.</returns>
-    inline std::error_code make_error_code(const NvAPI_Status e) noexcept {
+    inline std::error_code make_error_code(const nvmlReturn_t e) noexcept {
         return std::error_code(
             static_cast<int>(e),
-            PWROWG_DETAIL_NAMESPACE::nvapi_category());
+            PWROWG_DETAIL_NAMESPACE::nvml_category());
     }
 }
 
-#endif /* defined(POWER_OVERWHELMING_WITH_NVAPI) */
-#endif /* !defined(_PWROWG_NVAPI_ERROR_CATEGORY_H) */
+#endif /* defined(POWER_OVERWHELMING_WITH_NVML) */
+#endif /* !defined(_PWROWG_NVML_ERROR_CATEGORY_H) */
