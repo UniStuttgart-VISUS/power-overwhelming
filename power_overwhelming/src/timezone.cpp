58b1a3f90df42c5728f0dc9681429f88a7b2735b
Christoph <christoph.mueller@hotmail.de>
2026-06-19 09:44:08 +0200

    Fix bias again.

--- power_overwhelming/src/timezone.cpp
+++ power_overwhelming/src/timezone.cpp
@@ -7,8 +7,27 @@
 #include "timezone.h"
 
 
+/// <summary>
+/// Compare two <see cref="SYSTEMTIME" />s.
+/// </summary>
+static bool operator <(_In_ const SYSTEMTIME& lhs,
+    _In_ const SYSTEMTIME& rhs) {
+    FILETIME l, r;
+
+    if (!::SystemTimeToFileTime(&lhs, &l)) {
+        throw std::system_error(::GetLastError(), std::system_category());
+    }
+
+    if (!::SystemTimeToFileTime(&rhs, &r)) {
+        throw std::system_error(::GetLastError(), std::system_category());
+    }
+
+    return (::CompareFileTime(&l, &r) < 0);
+}
+
+
 /*
- * visus::power_overwhelming::detail::get_timezone_bias
+ * visus::power_overwhelming::detail
  */
 visus::power_overwhelming::timestamp::value_type
 visus::power_overwhelming::detail::get_timezone_bias(void) {
@@ -18,23 +37,50 @@ visus::power_overwhelming::detail::get_timezone_bias(void) {
         throw std::system_error(::GetLastError(), std::system_category());
     }
 
+    // Apply the current year to the transition dates to compare to current
+    // time.
+    SYSTEMTIME now;
+    GetSystemTime(&now);
+    tzi.DaylightDate.wYear = now.wYear;
+    tzi.StandardDate.wYear = now.wYear;
+
+    // If the month is zero, no DST is defined. Otherwise, we assume to be in
+    // DST and subsequently check whether this is actually the case.
+    auto dst = ((tzi.DaylightDate.wMonth != 0)
+        || (tzi.StandardDate.wMonth != 0));
+
+    if (dst) {
+        if (tzi.DaylightDate < tzi.StandardDate) {
+            // DST start and end are in the same year.
+            dst = ((tzi.DaylightDate < now) && (now < tzi.StandardDate));
+        } else {
+            // On the southern hemisphere, DST crosses the year boundary.
+            dst = !((tzi.StandardDate < now) && (now < tzi.DaylightDate));
+        }
+    }
+
     auto retval = static_cast<timestamp::value_type>(tzi.Bias);
 
-    if (tzi.DynamicDaylightTimeDisabled) {
+    if (dst) {
         retval += static_cast<timestamp::value_type>(tzi.DaylightBias);
     } else {
         retval += static_cast<timestamp::value_type>(tzi.StandardBias);
     }
 
-    retval *= 60LL * 1000LL * 10000LL;
+    // Convert to seconds.
+    retval *= 60LL;
 
-    return retval;
 #else /* defined(_WIN32) */
     static const time_t ts = 0;
     tm gmt;
     tm local;
     ::gmtime_r(&ts, &gmt);
     ::localtime_r(&ts, &local);
-    return ::mktime(&gmt) - ::mktime(&local);
+    timestamp::value_type retval = ::mktime(&gmt) - ::mktime(&local);
 #endif /* defined(_WIN32) */
+
+    // Convert seconds to 100 ns units.
+    retval *= 1000LL * 10000LL;
+
+    return retval;
 }
