b28fc7353fb0db7c2a8b5b2f85b8d6192a9359c7
Christoph <christoph.mueller@hotmail.de>
2026-06-26 17:50:36 +0200

    Added daqmx_configuration.

---
+++ power_overwhelming/include/visus/pwrowg/daqmx_configuration.h
@@ -0,0 +1,36 @@
+﻿// <copyright file="daqmx_configuration.h" company="Visualisierungsinstitut der Universität Stuttgart">
+// Copyright © 2026 Visualisierungsinstitut der Universität Stuttgart.
+// Licensed under the MIT licence. See LICENCE file for details.
+// </copyright>
+// <author>Christoph Müller</author>
+
+#if !defined(_PWROWG_DAQMX_CONFIGURATION_H)
+#define _PWROWG_DAQMX_CONFIGURATION_H
+#pragma once
+
+#include "visus/pwrowg/sensor_configuration.h"
+
+
+PWROWG_NAMESPACE_BEGIN
+
+/// <summary>
+/// Provides additional configuration options for NI-DAQmx-based sensors when
+/// creating a <see cref="sensor_array" />.
+/// </summary>
+class POWER_OVERWHELMING_API daqmx_configuration final
+        : public sensor_configuration {
+
+public:
+
+    /// <summary>
+    /// A unique identifier for the <see cref="daqmx_configuration" /> type.
+    /// </summary>
+    static const guid id;
+
+private:
+
+};
+
+PWROWG_NAMESPACE_END
+
+#endif /* !defined(_PWROWG_DAQMX_CONFIGURATION_H) */
---
+++ power_overwhelming/src/daqmx_configuration.cpp
@@ -0,0 +1,14 @@
+﻿// <copyright file="daqmx_configuration.cpp" company="Visualisierungsinstitut der Universität Stuttgart">
+// Copyright © 2026 Visualisierungsinstitut der Universität Stuttgart.
+// Licensed under the MIT licence. See LICENCE file for details.
+// </copyright>
+// <author>Christoph Müller</author>
+
+#include "visus/pwrowg/daqmx_configuration.h"
+
+
+/*
+ * PWROWG_NAMESPACE::daqmx_configuration::id
+ */
+const PWROWG_NAMESPACE::guid PWROWG_NAMESPACE::daqmx_configuration::id(
+    0x505ee24b, 0x3e1e, 0x43a2, 0xad15, 0x91, 0xbe, 0x37, 0x2a, 0x91, 0xbf);
