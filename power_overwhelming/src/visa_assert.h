2483cfbc3abdfcc436780ff189897f8a87a8578c
Christoph <christoph.mueller@hotmail.de>
2026-07-28 12:09:09 +0200

    Disable PWROWG_ASSERT_NO_VISA_ERROR by default, event in debug builds.

--- power_overwhelming/src/visa_assert.h
+++ power_overwhelming/src/visa_assert.h
@@ -13,10 +13,6 @@
 #include "visus/pwrowg/visa_instrument.h"
 
 
-#if ((defined(DEBUG) || defined(_DEBUG)) && !defined(NDEBUG))
-#define PWROWG_ASSERT_VISA_INSTRUMENT_STATE 1
-#endif /* ((defined(DEBUG) || defined(_DEBUG)) && !defined(NDEBUG)) */
-
 #if defined(PWROWG_ASSERT_VISA_INSTRUMENT_STATE)
 /// <summary>
 /// Asserts that the given instrument has no VISA error.
@@ -24,6 +20,8 @@
 /// <param name="instrument">The instrument to check.</param>
 #define PWROWG_ASSERT_NO_VISA_ERROR(instrument)\
     assert((instrument).system_error() == 0)
+#else /* !defined(PWROWG_ASSERT_VISA_INSTRUMENT_STATE) */
+#define PWROWG_ASSERT_NO_VISA_ERROR(instrument) ((void) 0)
 #endif /* defined(PWROWG_ASSERT_VISA_INSTRUMENT_STATE) */
 
 #endif /* !defined(_PWROWG_VISA_ASSERT_H) */
