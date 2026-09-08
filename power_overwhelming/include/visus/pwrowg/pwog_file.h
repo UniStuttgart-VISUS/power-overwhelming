636a1a9304af9dfa31a695bdc6cd754c8b7ef03c
Christoph <christoph.mueller@hotmail.de>
2026-09-08 18:03:12 +0200

    Make pwog_file public.

---
+++ power_overwhelming/include/visus/pwrowg/pwog_file.h
@@ -0,0 +1,103 @@
+﻿// <copyright file="pwog_file.h" company="Visualisierungsinstitut der Universität Stuttgart">
+// Copyright © 2026 Visualisierungsinstitut der Universität Stuttgart.
+// Licensed under the MIT licence. See LICENCE file for more information.
+// </copyright>
+// <author>Christoph Müller</author>
+
+#if !defined(_PWROWG_PWOG_FILE_H)
+#define _PWROWG_PWOG_FILE_H
+#pragma once
+
+#include <cinttypes>
+
+#if defined(_WIN32)
+#include <Windows.h>
+#endif /* defined(_WIN32) */
+
+#include "visus/pwrowg/api.h"
+
+
+PWROWG_NAMESPACE_BEGIN
+
+/// <summary>
+/// RAII object for interacting with a binary file format identified by the
+/// &quot;PWOG&quot; FOURCC.
+/// </summary>
+class PWROWG_TEST_API pwog_file final {
+
+public:
+
+    /// <summary>
+    /// The fourcc at the begin of each file.
+    /// </summary>
+    static constexpr const std::uint32_t fourcc = 'PWOG';
+
+    static pwog_file read(_In_z_ const wchar_t *path);
+
+    static pwog_file read(_In_z_ const char *path);
+
+    /// <summary>
+    /// Initialises a new instance.
+    /// </summary>
+    pwog_file(void) noexcept;
+
+    pwog_file(_Inout_ pwog_file&& rhs) noexcept;
+
+    /// <summary>
+    /// Finalises the instance.
+    /// </summary>
+    inline ~pwog_file(void) noexcept {
+        this->close();
+    }
+
+    /// <summary>
+    /// Close the file.
+    /// </summary>
+    void close(void) noexcept;
+
+    pwog_file& operator =(_Inout_ pwog_file&& rhs) noexcept;
+
+    /// <summary>
+    /// Indicates whether the file handle is valid.
+    /// </summary>
+    /// <returns><see langword="true"/> if the file handle is valid,
+    /// <see langword="false"/> otherwise.</returns>
+    inline operator bool(void) const noexcept {
+        return (this->_handle != invalid);
+    }
+
+private:
+
+#if defined(_WIN32)
+    typedef HANDLE handle_type;
+#else /* defined(_WIN32 */
+    typedef int handle_type;
+#endif /* defined(_WIN32 */
+
+#if defined(_WIN32)
+    static constexpr auto invalid = INVALID_HANDLE_VALUE;
+#else /* defined(_WIN32) */
+    static constexpr auto invalid = -1;
+#endif /* defined(_WIN32) */
+
+    /// <summary>
+    /// Reads the FOURCC, checks whether it is expected and whether the byte
+    /// order is the native one.
+    /// </summary>
+    /// <param name=""></param>
+    void check_fourcc(void);
+
+    /// <summary>
+    /// Reads the version number and checks whether it is supported.
+    /// </summary>
+    /// <param name=""></param>
+    void check_version(void);
+
+    handle_type _handle;
+    bool _swap;
+    std::uint8_t _version[2];
+};
+
+PWROWG_NAMESPACE_END
+
+#endif /* !defined(_PWROWG_PWOG_FILE_H) */
--- power_overwhelming/src/pwog_file.cpp
+++ power_overwhelming/src/pwog_file.cpp
@@ -4,4 +4,139 @@
 // </copyright>
 // <author>Christoph Müller</author>
 
-#include "pwog_file.h"
+#include "visus/pwrowg/pwog_file.h"
+
+#include <cassert>
+#include <memory>
+#include <stdexcept>
+#include <type_traits>
+#include <utility>
+
+#include "io_util.h"
+
+
+/*
+ * PWROWG_NAMESPACE::pwog_file::read
+ */
+PWROWG_NAMESPACE::pwog_file PWROWG_NAMESPACE::pwog_file::read(
+        _In_z_ const wchar_t *path) {
+    pwog_file retval;
+
+#if defined(_WIN32)
+    retval._handle = detail::open(path, GENERIC_READ, FILE_SHARE_READ,
+        OPEN_EXISTING);
+#else /* defined(_WIN32) */
+    retval._handle = detail::open(path, O_RDONLY | O_BINARY);
+#endif /* defined(_WIN32) */
+
+    retval.check_fourcc();
+    retval.check_version();
+
+    return retval;
+}
+
+
+/*
+ * PWROWG_NAMESPACE::pwog_file::read
+ */
+PWROWG_NAMESPACE::pwog_file PWROWG_NAMESPACE::pwog_file::read(
+        _In_z_ const char *path) {
+    pwog_file retval;
+
+#if defined(_WIN32)
+    retval._handle = detail::open(path, GENERIC_READ, FILE_SHARE_READ,
+        OPEN_EXISTING);
+#else /* defined(_WIN32) */
+    retval._handle = detail::open(path, O_RDONLY | O_BINARY);
+#endif /* defined(_WIN32) */
+
+    retval.check_fourcc();
+    retval.check_version();
+
+    return retval;
+}
+
+
+/*
+ * PWROWG_NAMESPACE::pwog_file::pwog_file
+ */
+PWROWG_NAMESPACE::pwog_file::pwog_file(void) noexcept
+    : _handle(invalid), _swap(false) { }
+
+
+/*
+ * PWROWG_NAMESPACE::pwog_file::pwog_file
+ */
+PWROWG_NAMESPACE::pwog_file::pwog_file(_Inout_ pwog_file&& rhs) noexcept
+        : _handle(rhs._handle), _swap(rhs._swap) {
+    rhs._handle = invalid;
+}
+
+
+/*
+ * PWROWG_NAMESPACE::pwog_file::close
+ */
+void PWROWG_NAMESPACE::pwog_file::close(void) noexcept {
+    if (*this) {
+#if defined(_WIN32)
+        ::CloseHandle(this->_handle);
+#else /* defined(_WIN32) */
+        ::close(this->_handle);
+#endif /* defined(_WIN32) */
+        this->_handle = invalid;
+    }
+}
+
+
+/*
+ * PWROWG_NAMESPACE::pwog_file::operator =
+ */
+PWROWG_NAMESPACE::pwog_file& PWROWG_NAMESPACE::pwog_file::operator =(
+        _Inout_ pwog_file&& rhs) noexcept {
+    if (this != std::addressof(rhs)) {
+        this->close();
+        assert(!*this);
+        std::swap(this->_handle, rhs._handle);
+        assert(!rhs);
+        this->_swap = rhs._swap;
+    }
+
+    return *this;
+}
+
+
+/*
+ * PWROWG_NAMESPACE::pwog_file::check_fourcc
+ */
+void PWROWG_NAMESPACE::pwog_file::check_fourcc(void) {
+    std::decay_t<decltype(fourcc)> actual = 0;
+    detail::read_bytes(this->_handle, &actual, sizeof(actual));
+
+    if (actual != fourcc) {
+        // If we did not find the expected FOURCC, it could be that the byte
+        // order is reversed. We check that next and, in case of success, we
+        // remember that we need to swap the byte order when reading.
+        auto b = reinterpret_cast<std::uint8_t *>(&actual);
+        auto e = b + sizeof(actual);
+        std::reverse(b, e);
+
+        if (actual != fourcc) {
+            throw std::invalid_argument("The specified file does not start "
+                "with the expected FOURCC.");
+        }
+
+        this->_swap = true;
+    }
+}
+
+
+/*
+ * PWROWG_NAMESPACE::pwog_file::check_version
+ */
+void PWROWG_NAMESPACE::pwog_file::check_version(void) {
+    detail::read_bytes(this->_handle, this->_version, sizeof(this->_version));
+    if ((this->_version[0] != 1) || (this->_version[1] != 0)) {
+        throw std::invalid_argument("The specified file has a version that the "
+            "reader does not understand.");
+    }
+}
